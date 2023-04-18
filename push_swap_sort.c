fd7776d2a68f842f44570bdd567755043b92e72d
jinsyang <jinsyang@student.42seoul.kr>
2023-04-18 19:20:03 +0900

    sor_a_5 try1

--- push_swap_sort.c
+++ push_swap_sort.c
@@ -6,7 +6,7 @@
 /*   By: jinsyang <jinsyang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 14:05:11 by jinsyang          #+#    #+#             */
-/*   Updated: 2023/04/17 19:20:57 by jinsyang         ###   ########.fr       */
+/*   Updated: 2023/04/18 19:18:32 by jinsyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,8 @@ void	sort_a_3(t_stack *stack)
 
 void	sort_a_5(t_stack *stack_a, t_stack *stack_b, int size)
 {
+	int	pivot;
+
 	while (size != 3)
 	{
 		ps_pab(stack_b, stack_a, 'b');
@@ -74,36 +76,57 @@ void	sort_a_5(t_stack *stack_a, t_stack *stack_b, int size)
 	sort_a_3(stack_a);
 	while (stack_b->next != NULL)
 	{
+		pivot = stack_a->next->data;
 		if (stack_a->next->data < stack_b->next->data &&
 			stack_a->next->next->data > stack_b->next->data)
 		{
 			ps_rab(stack_a, 'a');
 			ps_pab(stack_a, stack_b, 'a');
 		}
-		else // 여기도 수정해야할듯?
+		else
 			ps_rab(stack_a, 'a');
+		while(stack_b->next != NULL && stack_a->next->data != pivot)
+		{
+			if (stack_a->next->data < stack_b->next->data &&
+				stack_a->next->next->data > stack_b->next->data)
+			{
+				ps_rab(stack_a, 'a');
+				ps_pab(stack_a, stack_b, 'a');
+			}
+			else
+				ps_rab(stack_a, 'a');
+		}
+		if (stack_b->next != NULL)
+		{
+			ps_pab(stack_a, stack_b, 'a');
+			if (stack_a->next->data > stack_a->next->next->data)
+				ps_rrab(stack_a, 'a');
+		}
 	}
-	if (stack_b->next != NULL) // 2개있을떄 한꺼번에 처리 가능할꺼같은데 하드코딩하면
+	while (size)
 	{
-		ps_pab(stack_a, stack_b, 'a');
-		if (stack_a->next->data < stack_a->next->next->data)
-			ps_rab(stack_a, 'a');
-	}
+		ps_rrab(stack_a, 'a');
+		size--;
 	}
 }
 
 // 45123
-// 21345
-// 23145
+// 21345 !!
+// 51234
+// 23145 !!
 // 42135
 
 // 1 2 3 / 5 4 아에 다 작거나
 // 3 4 5 / 1 2 이에 다 크거나
+// 2 3 4 / 1 5 큰수 작은수 섞여있거나
 // 1 4 5 / 3 2 그냥 노말
 // 1 3 5 / 2 4 중간에 넣어야 하거나
 // 어쨋든 스택 뒤에 있는거랑 앞이랑 비교해서 그 다음 큰수가 나오면 옮기기
+// rra 돌리기? 아니면 2개 판단하기
 
-// 4 3 2 1
+
+
+
 
 void	sort_stack(t_stack *stack_a, t_stack *stack_b, int size)
 {
