af5f5d076ebec431eea583ccd5a01cb6d685008c
jinsyang <jinsyang@student.42seoul.kr>
2023-04-20 19:32:15 +0900

    sor_a_5 try2

--- push_swap_sort.c
+++ push_swap_sort.c
@@ -6,7 +6,7 @@
 /*   By: jinsyang <jinsyang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 14:05:11 by jinsyang          #+#    #+#             */
-/*   Updated: 2023/04/18 19:18:32 by jinsyang         ###   ########.fr       */
+/*   Updated: 2023/04/20 19:27:11 by jinsyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,9 @@ void	sort_a_3(t_stack *stack)
 
 void	sort_a_5(t_stack *stack_a, t_stack *stack_b, int size)
 {
-	int	pivot;
+	t_list	*tmp;
 
+	tmp = stack_a->next;
 	while (size != 3)
 	{
 		ps_pab(stack_b, stack_a, 'b');
@@ -76,38 +77,16 @@ void	sort_a_5(t_stack *stack_a, t_stack *stack_b, int size)
 	sort_a_3(stack_a);
 	while (stack_b->next != NULL)
 	{
-		pivot = stack_a->next->data;
-		if (stack_a->next->data < stack_b->next->data &&
-			stack_a->next->next->data > stack_b->next->data)
+		if (stack_b->next->data > stack_a->next->data && stack_b->next->data < stack_a->next->next->data)
 		{
 			ps_rab(stack_a, 'a');
-			ps_pab(stack_a, stack_b, 'a');
+			ps_pab(stack_a, stack_b, 'b');
 		}
 		else
 			ps_rab(stack_a, 'a');
-		while(stack_b->next != NULL && stack_a->next->data != pivot)
-		{
-			if (stack_a->next->data < stack_b->next->data &&
-				stack_a->next->next->data > stack_b->next->data)
-			{
-				ps_rab(stack_a, 'a');
-				ps_pab(stack_a, stack_b, 'a');
-			}
-			else
-				ps_rab(stack_a, 'a');
-		}
-		if (stack_b->next != NULL)
-		{
-			ps_pab(stack_a, stack_b, 'a');
-			if (stack_a->next->data > stack_a->next->next->data)
-				ps_rrab(stack_a, 'a');
-		}
 	}
-	while (size)
-	{
+	while (stack_a->next->next->next->next->next->data < stack_a->next->data)
 		ps_rrab(stack_a, 'a');
-		size--;
-	}
 }
 
 // 45123
@@ -123,10 +102,6 @@ void	sort_a_5(t_stack *stack_a, t_stack *stack_b, int size)
 // 1 3 5 / 2 4 중간에 넣어야 하거나
 // 어쨋든 스택 뒤에 있는거랑 앞이랑 비교해서 그 다음 큰수가 나오면 옮기기
 // rra 돌리기? 아니면 2개 판단하기
-
-
-
-
 
 void	sort_stack(t_stack *stack_a, t_stack *stack_b, int size)
 {
