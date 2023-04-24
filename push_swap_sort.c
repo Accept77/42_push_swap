3657d7f750c3778e2bd3fae86783207970e9704a
jinsyang <jinsyang@student.42seoul.kr>
2023-04-24 18:55:20 +0900

    try5

--- push_swap_sort.c
+++ push_swap_sort.c
@@ -6,7 +6,7 @@
 /*   By: jinsyang <jinsyang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 14:05:11 by jinsyang          #+#    #+#             */
-/*   Updated: 2023/04/20 19:27:11 by jinsyang         ###   ########.fr       */
+/*   Updated: 2023/04/24 18:41:23 by jinsyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,28 @@ int	part(t_stack *stack_a, t_stack *stack_b, int size)
 	return (ra_count);
 }
 
+int	is_max_min(t_stack *stack_a, t_stack *stack_b)
+{
+	int		max_min;
+	int		flag;
+	t_list	*tmp_a;
+
+	flag = 0;
+	tmp_a = stack_a->next;
+	max_min = 0;
+	while (tmp_a != NULL)
+	{
+		if (stack_b->next->data > tmp_a->data)
+			max_min++;
+		else if (stack_b->next->data < tmp_a->data)
+			max_min--;
+		tmp_a = tmp_a->next;
+	}
+	if (max_min == 3 || max_min == -3)
+		flag++;
+	return (flag);
+}
+
 void	sort_a_2(t_stack *stack)
 {
 	t_list	*tmp;
@@ -67,6 +89,7 @@ void	sort_a_3(t_stack *stack)
 void	sort_a_5(t_stack *stack_a, t_stack *stack_b, int size)
 {
 	t_list	*tmp;
+	int		flag;
 
 	tmp = stack_a->next;
 	while (size != 3)
@@ -77,23 +100,39 @@ void	sort_a_5(t_stack *stack_a, t_stack *stack_b, int size)
 	sort_a_3(stack_a);
 	while (stack_b->next != NULL)
 	{
-		if (stack_b->next->data > stack_a->next->data && stack_b->next->data < stack_a->next->next->data)
+		flag = is_max_min(stack_a, stack_b);
+		while (stack_b->next != NULL && flag == 0)
 		{
-			ps_rab(stack_a, 'a');
-			ps_pab(stack_a, stack_b, 'b');
+			if (stack_b->next->data > stack_a->next->data &&
+				stack_b->next->data < stack_a->next->next->data)
+			{
+				ps_rab(stack_a, 'a');
+				ps_pab(stack_a, stack_b, 'b');
+			}
+			else
+				ps_rab(stack_a, 'a');
 		}
-		else
-			ps_rab(stack_a, 'a');
+		if (stack_b->next != NULL)
+		{
+			ps_pab(stack_a, stack_b, 'a');
+			if (stack_a->next->data < stack_a->next->next->data)
+				ps_rrab(stack_a, 'a');
+		}
 	}
 	while (stack_a->next->next->next->next->next->data < stack_a->next->data)
 		ps_rrab(stack_a, 'a');
 }
 
 // 45123
-// 21345 !!
-// 51234
-// 23145 !!
+// 21345
+// 51234 !!!
+// 23145
 // 42135
+
+// 큰거 처리하기
+// 어떤 방법으로??
+// 큰거를 판단해서 마지막에 돌아가거나 큰거면 바로 넘겨버리고 돌리기
+// 판단 방법은?
 
 // 1 2 3 / 5 4 아에 다 작거나
 // 3 4 5 / 1 2 이에 다 크거나
